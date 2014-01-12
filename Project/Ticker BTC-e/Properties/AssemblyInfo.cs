bf330a2e468f29f509e82b23962b8a613c7de52b
AnimaLocus <private@animalocus.com>
2014-01-12 14:51:45 +0600

    New name and new build.

--- Project/Ticker BTC-e/Properties/AssemblyInfo.cs
+++ Project/Ticker BTC-e/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ticker BTC-e")]
+[assembly: AssemblyTitle("True Terminal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ticker BTC-e")]
+[assembly: AssemblyProduct("True Terminal")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
